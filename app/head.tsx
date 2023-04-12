498fdf1e31f4f62fe5cdaf15b7e343cd3ccf4fb6
AfonsoDev <peresfon@gmail.com>
2023-04-12 00:11:32 -0300

    first commit

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>LinkTree - AfonsoDev</title>
+        <meta name='description' content='Next.js + Tailwind CSS' />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+    </>
+  )
+}
